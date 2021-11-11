9405be76464807fcb46ecdf1d0e051a95a282b5b
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2021-11-11 18:51:24 -0300

    add functions to read mesabinary output

--- pkg/read_file.go
+++ pkg/read_file.go
@@ -27,10 +27,16 @@ type MESAstar_info struct {
 
 // struct holding info on MESAbinary
 type MESAbinary_info struct {
-   period float64
+   model_number int
+   initial_don_mass, initial_acc_mass float64
+   initial_period float64
+   age float64
    star_1_mass, star_2_mass float64
+   period float64
    MT_case string
    History_name string
+   donor_index, point_mass_index int
+   rel_rl_1, rel_rl_2 float64
 }
 
 
@@ -255,5 +261,196 @@ func Grab_star_run_info (fname string, star_info *MESAstar_info) {
 
    if err := scanner.Err(); err != nil {
       log.Fatal(err)
+   }
+}
+
+
+func Grab_binary_header (fname string, binary_info *MESAbinary_info) {
+
+   // MESA specfic row numbers for header names & values in history output
+   nr_header_names := 2
+   nr_header_values := 3
+
+   // open binary file
+   fbinary, err := os.Open(fname)
+   if err != nil {log.Fatal(err)}
+   defer fbinary.Close()
+
+   // scan star file
+   scanner := bufio.NewScanner(fbinary)
+
+   // arrays holding star header names & values
+   var header_names []string
+   var header_values []string
+   var header_names_found, header_values_found bool
+   lineCount := 0
+
+   for scanner.Scan() {
+
+      lineCount++
+
+      // get header names
+      if lineCount == nr_header_names {
+         header_names = strings.Fields(scanner.Text())
+         header_names_found = true
+      }
+
+      // find header values
+      if lineCount == nr_header_values {
+         header_values = strings.Fields(scanner.Text())
+         header_values_found = true
+      }
+
+      if (header_names_found && header_values_found) {
+         for k, name := range header_names {
+            val := header_values[k]
+            if name == "initial_don_mass" {
+               i, err := strconv.ParseFloat(val, 64)  // i is in Msun
+               if err != nil {
+                  fmt.Println(err)
+                  os.Exit(2)
+               }
+               binary_info.initial_don_mass = i
+            }
+            if name == "initial_acc_mass" {
+               i, err := strconv.ParseFloat(val, 64)  // i is in Msun
+               if err != nil {
+                  fmt.Println(err)
+                  os.Exit(2)
+               }
+               binary_info.initial_acc_mass = i
+            }
+            if name == "initial_period_days" {
+               i, err := strconv.ParseFloat(val, 64)  // i is in days
+               if err != nil {
+                  fmt.Println(err)
+                  os.Exit(2)
+               }
+               binary_info.initial_period = i
+            }
+         }
+      }
+   }
+
+   if err := scanner.Err(); err != nil {
+      log.Fatal(err)
+   }
+}
+
+
+func Grab_binary_run_info (fname string, binary_info *MESAbinary_info) {
+
+   nr_column_names := 6
+
+   // open star file
+   fbinary, err := os.Open(fname)
+   if err != nil {log.Fatal(err)}
+   defer fbinary.Close()
+
+   // scan star file
+   scanner := bufio.NewScanner(fbinary)
+
+   // arrays holding star header names & values
+   var column_names []string
+   var column_values []string
+   var column_names_found bool
+   lineCount := 0
+
+   for scanner.Scan() {
+
+      lineCount++
+
+      // get header names
+      if lineCount == nr_column_names {
+         column_names = strings.Fields(scanner.Text())
+         column_names_found = true
+      }
+
+      if column_names_found {break}
+   }
+
+   if (column_names_found) {
+      column_values = strings.Fields(getLastLineWithSeek(fname))
+   }
+
+   if (column_names_found) {
+      for k, name := range column_names {
+
+         val := column_values[k]
+
+         if name == "model_number" {
+            i, err := strconv.Atoi(val)
+            // handle error
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.model_number = i
+         }
+         if name == "age" {
+            i, err := strconv.ParseFloat(val, 64)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.age = i
+         }
+         if name == "period_days" {
+            i, err := strconv.ParseFloat(val, 64)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.period = i
+         }
+         if name == "star_1_mass" {
+            i, err := strconv.ParseFloat(val, 64)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.star_1_mass = i
+         }
+         if name == "star_2_mass" {
+            i, err := strconv.ParseFloat(val, 64)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.star_2_mass = i
+         }
+         if name == "donor_index" {
+            i, err := strconv.Atoi(val)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.donor_index = i
+         }
+         if name == "point_mass_index" {
+            i, err := strconv.Atoi(val)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.point_mass_index = i
+         }
+         if name == "rl_relative_overflow_1" {
+            i, err := strconv.ParseFloat(val, 64)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.rel_rl_1 = i
+         }
+         if name == "rl_relative_overflow_2" {
+            i, err := strconv.ParseFloat(val, 64)
+            if err != nil {
+               fmt.Println(err)
+               os.Exit(2)
+            }
+            binary_info.rel_rl_2 = i
+         }
+      }
    }
 }
