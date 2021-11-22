b8181684da1d6b29e12485d1b0291b27f6f2b0c8
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2021-11-22 15:25:02 -0300

    re-structure main package for new modules

--- cmd/mesa-state-monitor/main.go
+++ cmd/mesa-state-monitor/main.go
@@ -1,58 +1,14 @@
 package main
 
+
 import (
    "fmt"
-   "html/template"
    "log"
-	"net/http"
 	"os"
 
    "mesa-state-monitor/pkg/io"
-   "mesa-state-monitor/pkg/utils"
+   "mesa-state-monitor/pkg/web"
 )
-
-var tpl_mesastar = template.Must(template.ParseFiles("web/index-mesastar.html"))
-var tpl_mesabinary = template.Must(template.ParseFiles("web/index-mesabinary.html"))
-
-
-type wrapperStruct struct {
-   binaryFilename string
-   star1Filename string
-   star2Filename string
-}
-
-
-// index html for MESAstar 
-func (ws wrapperStruct) indexHandler_mesastar(w http.ResponseWriter, r *http.Request) {
-
-   starfilePath := ws.star1Filename
-
-   // create struct of MESAstar_info
-   info := new(io.MESAstar_info)
-   info.History_name = starfilePath
-   io.Grab_star_header(starfilePath, info)
-   io.Grab_star_run_info(starfilePath, info)
-   info.Evol_state = utils.SetEvolutionaryStage(info.Mass, info.Center_h1, info.Center_he4, info.Log_T_cntr)
-
-   tpl_mesastar.Execute(w, info)
-}
-
-
-// index html for MESAbinary
-func (ws wrapperStruct) indexHandler_mesabinary(w http.ResponseWriter, r *http.Request) {
-
-   binaryfilePath := ws.binaryFilename
-
-   // create struct of MESAbinary_info   
-   binfo := new(io.MESAbinary_info)
-   binfo.History_name = ""
-   binfo.MT_case = "none"
-   io.Grab_binary_header(binaryfilePath, binfo)
-   io.Grab_binary_run_info(binaryfilePath, binfo)
-   // binfo.MT_case = utils.SetMTCase(binfo.Radius1, binfo.RLobe1, info.EvolState)
-
-   tpl_mesabinary.Execute(w, nil)
-}
 
 
 func main() {
@@ -89,33 +45,11 @@ func main() {
    binaryLogName, star1LogName, star2LogName := io.GetLogNames(mesaRoot, isBinaryEvolution)
 
    // create the wrapper struct from which the server will be launched
-   handlers := wrapperStruct{
-         binaryFilename: binaryLogName,
-         star1Filename: star1LogName,
-         star2Filename: star2LogName}
+   handlers := web.WrapperStruct{
+         BinaryFilename: binaryLogName,
+         Star1Filename: star1LogName,
+         Star2Filename: star2LogName}
 
+   web.Start(isBinaryEvolution, &handlers)
 
-   // set port number for the webpage
-   port := os.Getenv("PORT")
-	if port == "" {port = "3000"}
-
-   // configs
-	mux := http.NewServeMux()
-   fs := http.FileServer(http.Dir("./web/assets/"))
-   mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-
-   // serve
-   if (isBinaryEvolution) {
-
-      // open http server with template of MESAstar (single evolution)
-      mux.HandleFunc("/", handlers.indexHandler_mesabinary)
-
-   } else {
-
-      // open http server with template of MESAbinary (binary evolution)
-      mux.HandleFunc("/", handlers.indexHandler_mesastar)
-
-   }
-
-   http.ListenAndServe(":"+port, mux)
 }
